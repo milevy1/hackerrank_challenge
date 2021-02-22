27e75f07977fa64aaaac3518ae024b7a82d08f3a
Matt Levy <milevy1@gmail.com>
2021-02-22 17:42:15 -0600

    Adds psuedocode of planned solution

--- lib/hackerrank_challenge.rb
+++ lib/hackerrank_challenge.rb
@@ -1,3 +1,11 @@
 class Hackerrank_Challenge
+    def topArticles(username, limit)
+        # Get all pages of articles for the user
 
+        # Sort by num_comments
+
+        # Map a list of the title names (story_title if null)
+
+        # Limit above list to the input limit
+    end
 end
--- spec/hackerrank_challenge_spec.rb
+++ spec/hackerrank_challenge_spec.rb
@@ -1,4 +1,10 @@
 require 'hackerrank_challenge'
 
 RSpec.describe Hackerrank_Challenge do
+    describe 'topArticles' do
+        it 'should return top N articles by a user' do
+            subject = Hackerrank_Challenge.new
+            expect(subject.topArticles('olalonde', 1)).to eq(10)
+        end
+    end
 end
