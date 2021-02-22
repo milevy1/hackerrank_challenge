5762d7962ab1331c64500e653730bca542b16db2
Matt Levy <milevy1@gmail.com>
2021-02-22 17:47:05 -0600

    Adds test for getAllArticles for a single page

--- spec/hackerrank_challenge_spec.rb
+++ spec/hackerrank_challenge_spec.rb
@@ -1,10 +1,22 @@
 require 'hackerrank_challenge'
 
 RSpec.describe Hackerrank_Challenge do
+    before :each do
+        subject = Hackerrank_Challenge.new
+    end
+
     describe 'topArticles' do
-        it 'should return top N articles by a user' do
-            subject = Hackerrank_Challenge.new
+        xit 'should return top N articles by a user' do
             expect(subject.topArticles('olalonde', 1)).to eq(10)
+        end
+    end
+
+    describe 'getAllArticles' do
+        it 'should return all articles for a given user if only 1 page' do
+             stub_request(:get, /jsonmock/)
+                .to_return(status: 200, body: File.read('mockResponse_1page.json'))
+
+            expect(subject.getAllArticles('olalonde')).to eq(["Show HN: This up votes itself"])
         end
     end
 end
